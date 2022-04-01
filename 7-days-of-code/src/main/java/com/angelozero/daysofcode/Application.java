ded8b9bd947a7b9cb00984f1c849a2f684cb18f6
Angelo Zero <angelo.akm@gmail.com>
2022-04-01 19:40:46 -0300

    creating a list of lists with parameters and their values

--- 7-days-of-code/src/main/java/com/angelozero/daysofcode/Application.java
+++ 7-days-of-code/src/main/java/com/angelozero/daysofcode/Application.java
@@ -2,14 +2,16 @@ package com.angelozero.daysofcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCaching
 @EnableFeignClients
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
--- 7-days-of-code/src/main/java/com/angelozero/daysofcode/usecase/GetTop250Movies.java
+++ 7-days-of-code/src/main/java/com/angelozero/daysofcode/usecase/GetTop250Movies.java
@@ -6,6 +6,7 @@ import com.angelozero.daysofcode.usecase.domain.ImdbDomain;
 import com.angelozero.daysofcode.usecase.mapper.ImdbUseCaseMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,8 +21,10 @@ public class GetTop250Movies {
     private final ImdbProperties imdbProperties;
     private final ImdbUseCaseMapper imdbUseCaseMapper;
 
+    @Cacheable("imdb250MoviesCache")
     public List<ImdbDomain> execute() {
         try {
+            log.info("\n[INFO] - Getting the 250 top movies\n");
             return imdbApiClient.getTop250Movies(imdbProperties.getApiKey())
                     .getItems()
                     .stream()
@@ -29,7 +32,7 @@ public class GetTop250Movies {
                     .collect(Collectors.toList());
 
         } catch (Exception ex) {
-            log.error("\nERRO IMDB_API_CLIENT: " + ex.getMessage() + "\n");
+            log.error("\n[ERROR] - IMDB API CLIENT: " + ex.getMessage() + "\n");
             throw new RuntimeException(ex);
         }
     }
